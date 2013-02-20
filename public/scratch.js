7de083c66153d8b9bc1f719c23056211fcd5b2e4
Ryan Buckley <arebuckley@gmail.com>
2013-02-19 23:43:34 -1000

    Google maps api

---
+++ public/scratch.js
@@ -0,0 +1,20 @@
+(new google.maps.DirectionsService()).route({
+    origin: new google.maps.LatLng(20.886570, -156.475375),
+    destination: new google.maps.LatLng(20.8943, -156.4377),
+    waypoints: [
+        {
+            location: new google.maps.LatLng(20.883693, -156.471705)
+        },
+        {
+            location: new google.maps.LatLng(20.88789, -156.4676064)
+        }
+    ],
+    travelMode: 'DRIVING'
+}, function (result, status) {
+    var x = google.maps.geometry.encoding.encodePath(result.routes[0].overview_path);
+    console.log(x)
+});
+/*
+URL for a static map fof upcountry islander
+http://maps.googleapis.com/maps/api/staticmap?zoom=14&size=600x600&sensor=false&maptype=roadmap&path=weight:3|enc:_in~Bzy`z\dAUt@Ml@EtAIb@Df@RHKJ_@d@mBPmANcBNgCP{CNmIBs@{@]sBq@uA[eEe@cAMoAe@q@a@c@c@i@{@e@mASu@qA_Gj@Qk@PqA{FuA}FcA_EcBsHOc@Ua@}@_AqA}@_BaAxAaCfFkIlAoBzD}G`C{DlDoG~AcDnCmF[KoE}AiE_Bc@_@g@q@]u@yBuFgEyK{GePaBiEeAuByAkDk@aBw@eBAW?o@Da@tC}EN_@F]Ag@Ci@MYeJgGw@m@
+ */
