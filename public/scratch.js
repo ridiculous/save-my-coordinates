fd15e7a2c1a05d3bf0d3cb45d6272883101b4d76
Ryan Buckley <rbuckley@hawaii.edu>
2013-02-20 14:19:50 -1000

    Update public/scratch.js

--- public/scratch.js
+++ public/scratch.js
@@ -14,6 +14,9 @@
     var x = google.maps.geometry.encoding.encodePath(result.routes[0].overview_path);
     console.log(x)
 });
+// get current users location
+var t = navigator.geolocation.getCurrentPosition(function(a) { alert(a) })
+t.coords.latitude + ','+ t.coords.longitude
 /*
 URL for a static map fof upcountry islander
 http://maps.googleapis.com/maps/api/staticmap?zoom=14&size=600x600&sensor=false&maptype=roadmap&path=weight:3|enc:_in~Bzy`z\dAUt@Ml@EtAIb@Df@RHKJ_@d@mBPmANcBNgCP{CNmIBs@{@]sBq@uA[eEe@cAMoAe@q@a@c@c@i@{@e@mASu@qA_Gj@Qk@PqA{FuA}FcA_EcBsHOc@Ua@}@_AqA}@_BaAxAaCfFkIlAoBzD}G`C{DlDoG~AcDnCmF[KoE}AiE_Bc@_@g@q@]u@yBuFgEyK{GePaBiEeAuByAkDk@aBw@eBAW?o@Da@tC}EN_@F]Ag@Ci@MYeJgGw@m@
